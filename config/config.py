6a2d774d28c2cc6d8a5bcf2ae6bfa44af0f7b27c
rffefrrrrrrr <yassibohdaid@gmail.com>
2025-07-10 01:05:27 +0100

    Update config.py

--- config/config.py
+++ config/config.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 # تعيين المتغيرات مباشرة إذا لم يتم العثور عليها في ملف .env
 # رمز البوت - يستخدم في bot.py كـ TELEGRAM_BOT_TOKEN
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7820951703:AAGXTxYNaZLElwRy8t7S_9E1ny56_Hfn6L0")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # معرف المستخدم المسؤول
 ADMIN_USER_ID = int(os.getenv("ADMIN_USER_ID", "6459577996"))
